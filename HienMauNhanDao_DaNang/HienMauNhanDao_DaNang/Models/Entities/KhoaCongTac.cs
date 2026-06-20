2a3693aad4f0e4b87b8d6c5ee63b2d310d66dca5
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-21 02:16:52 +0700

    Viết entities cho KhoaCongTac

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/Entities/KhoaCongTac.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HienMauNhanDao_DaNang.Models.Entities
+{
+    [Table("KhoaCongTac")]
+    public class KhoaCongTac
+    {
+
+        [Key]
+        [Column("maKhoa")]
+        [MaxLength(10)]
+        public string MaKhoa { set; get; } = string.Empty;
+
+        [Required]
+        [Column("tenKhoa")]
+        [MaxLength(100)]
+        public string TenKhoa { set; get; } = string.Empty;
+
+
+    }
+}
