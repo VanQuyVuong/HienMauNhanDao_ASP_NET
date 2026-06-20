4f477c5e768ee17acd128f5ab88694ef0938242e
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-21 02:13:09 +0700

    Viết entities cho TaiKhoan

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/Entities/TaiKhoan.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HienMauNhanDao_DaNang.Models.Entities
+{
+    [Table("TaiKhoan")]
+    public class TaiKhoan
+    {
+
+        [Key]
+        [Column("maTaiKhoan")]
+        [MaxLength(10)]
+        public string maTaiKhoan = string.Empty;
+
+        // --- KHÓA NGOẠI (Foreign Key) ---
+        // Cột này lưu GIÁ TRỊ của maVaiTro trong bảng VAITRO
+        [Column("maVaiTro")]
+        [MaxLength(10)]
+        public string? MaVaiTro { get; set; }
+
+
+        // --- NAVIGATION PROPERTY ---
+        // KHÔNG phải cột trong DB
+        // Dùng để truy cập object VaiTro từ TaiKhoan
+        [ForeignKey("maVaiTro")]
+        public VaiTro? VaiTro { get; set; }
+
+        [Required]
+        [Column("email")]
+        [MaxLength(100)]
+        public string Email { set; get; } = string.Empty;
+
+        [Required]
+        [Column("matKhau")]
+        public string MatKhau { set; get; } = string.Empty;
+
+
+        // bool = true/false, mặc định true = tài khoản đang hoạt động
+        [Column("trangThai")]
+        public bool TrangThai { set; get; } = true;
+        
+    }
+}
