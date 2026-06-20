481bf148dec564734c63441b40ca1b91ae42c2a8
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-21 01:37:18 +0700

    Tạo entities Vaitro

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/Entities/VaiTro.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HienMauNhanDao_DaNang.Models.Entities
+{
+    [Table("VaiTro")]
+    public class VaiTro
+    {
+        [Key]
+        [Column("MaVaiTro")]
+        [MaxLength(10)]
+        public String maVaiTro { get; set; } = String.Empty;
+
+        [Required]
+        [Column("TenVaiTro")]
+        [MaxLength(50)]
+        public string tenVaiTro { get; set; } = string.Empty;
+    }
+}
