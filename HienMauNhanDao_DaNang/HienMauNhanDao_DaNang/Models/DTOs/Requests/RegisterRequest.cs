710cf4cfb767394f7a524a127b4d58a95b1ba908
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-23 01:30:51 +0700

    Tạo và viết RgisterRequest và LoginReponse

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/DTOs/Requests/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HienMauNhanDao_DaNang.Models.DTOs.Requests
+{
+    public class RegisterRequest
+    {
+
+        [Required(ErrorMessage = "Eamil khong duoc de trong")]
+        [EmailAddress(ErrorMessage = "Email khong dung dinh dang")]
+        public string Email { set; get; } = string.Empty;
+
+        [Required(ErrorMessage = "Mat khau khong duoc de trong")]
+        [MinLength(6, ErrorMessage = "Mat khau phai tu 6 ky tu tro len")]
+        [MaxLength(50, ErrorMessage = "Mat khau khong qua 50 ky tu")]
+        public string MatKhau { set; get; } = string.Empty;
+    }
+}
---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HienMauNhanDao_DaNang.Models.DTOs.Responses
+{
+    public class LoginResponse
+    {
+        // [JsonPropertyName] = đổi tên field khi trả về JSON
+        // C# dùng PascalCase (AccessToken)
+        // nhưng JSON trả về sẽ là snake_case (access_token)
+
+        [JsonPropertyName("access_token")]
+        public string AccessToken { set; get; } = string.Empty;
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { set; get; } = string.Empty;
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = "Bearer";
+
+        [JsonPropertyName("expires_in")]
+        public long ExpiresIn { set; get; } = 900; // 15 phút = 900 giây
+
+        [JsonPropertyName("user_id")]
+        public string UserId { set; get; } = string.Empty;
+        public string Email { set; get; } = string.Empty;
+        public string MaVaiTro { set; get; } = string.Empty;
+        public string? MaNhanVien { set; get; }//nullable vì không phải ai cũng là nhân viên
+        public string? MaTNV { set; get; }
+    }
+}
