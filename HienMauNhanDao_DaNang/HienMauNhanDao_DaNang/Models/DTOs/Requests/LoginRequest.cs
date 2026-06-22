71fb410b02465e3875eee981a57535d366754336
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-23 01:17:51 +0700

    Create Login request in DTOs/Request

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Models/DTOs/Requests/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HienMauNhanDao_DaNang.Models.DTOs.Requests
+{
+    public class LoginRequest
+    {
+
+        [Required(ErrorMessage = "Email khong duoc de trong")]
+        [EmailAddress(ErrorMessage = "Email khong dung dinh dang")]
+        public string Email { set; get; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Mat khau khong duoc de trong")]
+        [MinLength(6, ErrorMessage = "Mat khau phai tu 6 ky tu tro len")]
+        public string MatKhau { get; set; } = string.Empty;
+    }
+}
