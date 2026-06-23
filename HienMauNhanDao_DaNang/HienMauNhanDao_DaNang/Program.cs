7f32114cb3624c493d91624746142772c396a1f1
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-23 22:41:03 +0700

    Cấu hình JWT

--- HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Program.cs
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Program.cs
@@ -38,10 +38,38 @@ namespace HienMauNhanDao_DaNang
                 });
 
 
-            //Kết nối với Database
+            //3.Kết nối với Database
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDbContext>(options => 
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            var jwtConfig = builder.Configuration.GetSection("Jwt");
+            var secretKey = Encoding.UTF8.GetBytes(jwtConfig["Secret"]!);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtConfig["Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = jwtConfig["Audience"],
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+
+
+
         }
     }
 }
