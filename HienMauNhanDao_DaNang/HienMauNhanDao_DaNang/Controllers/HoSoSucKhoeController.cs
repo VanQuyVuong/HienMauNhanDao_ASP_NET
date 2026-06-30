935cb44a63370b95744a5e394e668db27e1d61f7
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-30 23:54:02 +0700

    feat(backend): khoi tao HoSoSucKhoeController va API tao ho so y te

---
+++ HienMauNhanDao_DaNang/HienMauNhanDao_DaNang/Controllers/HoSoSucKhoeController.cs
@@ -0,0 +1,72 @@
+﻿using HienMauNhanDao_DaNang.Data;
+using HienMauNhanDao_DaNang.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HienMauNhanDao_DaNang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HoSoSucKhoeController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public HoSoSucKhoeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public class HoSoRequest
+        {
+            public string MaDon { get; set; } = string.Empty;
+            public bool KhangSinh { get; set; }
+            public bool TruyenNhiem { get; set; }
+            public bool DauHong { get; set; }
+            public bool CoThai { get; set; }
+            public string? MoTaKhac { get; set; }
+        }
+
+
+        //Api 1 tạo hồ sơ khai báo sức khoẻ mới 
+        public async Task<IActionResult> CreateHoSo(HoSoRequest request)
+        {
+            //kiểm tra đơn hiến máu có tồn tại không
+            var don = await _context.DonDangKys.FindAsync(request.MaDon);
+            if(don == null)
+            {
+                return NotFound(new { success = false, message = "Không tìm thấy đơn đăng ký hiến máu" });
+            }
+
+            //kiểm tra xem đưuojc này đã được khai báo y tế chưa 
+            var daCo = await _context.HoSoSucKhoes.AnyAsync(h => h.MaDon == request.MaDon);
+            if (daCo)
+            {
+                return BadRequest(new { success = false, message = "Đơn đăng ký này đã được khai báo y tế trước đo rồi !" });
+            }
+
+            //Tạo mã hồ sơ tăng dạng HS00001, HS00002,...
+            var count = await _context.HoSoSucKhoes.CountAsync();
+            var maHoSo = $"HS{(count+1):DS}";
+
+            var hoSo = new HoSoSucKhoe
+            {
+                MaHoSo = maHoSo,
+                MaDon = request.MaDon,
+                KhangSinh = request.KhangSinh,
+                TruyenNhiem = request.TruyenNhiem,
+                DauHong = request.DauHong,
+                CoThai = request.CoThai,
+                MoTaKhac = request.MoTaKhac
+            };
+
+            _context.HoSoSucKhoes.Add(hoSo);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { success = true, message = "Lưu hồ sơ khai báo y tế thành công !", data = hoSo });
+        }
+    }
+}
