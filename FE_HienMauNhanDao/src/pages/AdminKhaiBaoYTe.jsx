ad140c057d214039abb7cc9c9decaad6b2c42b1b
VANQUYVUONG <vanbin1605@gmail.com>
2026-07-16 00:18:45 +0700

    fix(frontend): import CSS cho trang danh sach khai bao y te de sua loi vo layout close #33

--- FE_HienMauNhanDao/src/pages/AdminKhaiBaoYTe.jsx
+++ FE_HienMauNhanDao/src/pages/AdminKhaiBaoYTe.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import "../css/AdminKhaiBaoYTe.css";
 
 export default function AdminKhaiBaoYTe() {
   const [danhSach, setDanhSach] = useState([]);
